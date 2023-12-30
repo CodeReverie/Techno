d35beca848da1bfdf4a82d996f7bed59eba9b24f
CodeReverie <eneriajohnrenz@gmail.com>
2023-12-30 23:56:10 +0800

    9 roles

--- LoginTest/Assets/Scripts/ToLobby.cs
+++ LoginTest/Assets/Scripts/ToLobby.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class ToLobby : MonoBehaviour
 {
-     public void LoadScene()
+    public void LoadScene()
     {
+        // Check if connected to Photon before loading the scene
+        if (PhotonNetwork.IsConnected)
+        {
+            // Disconnect from Photon
+            PhotonNetwork.Disconnect();
+        }
+
+        // Load the scene
         SceneManager.LoadScene("FirebaseAuth");
     }
 }
